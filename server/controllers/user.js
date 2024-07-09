8b0930ce9e16145bc87bcb99dcb42030e73eebd5
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2024-07-09 21:20:19 +0530

    done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -133,34 +133,34 @@ export const forgotPassword = TryCatch(async (req, res) => {
   });
 });
 
-// export const resetPassword = TryCatch(async (req, res) => {
-//   const decodedData = jwt.verify(req.query.token, process.env.Forgot_Secret);
+export const resetPassword = TryCatch(async (req, res) => {
+  const decodedData = jwt.verify(req.query.token, process.env.Forgot_Secret);
 
-//   const user = await User.findOne({ email: decodedData.email });
+  const user = await User.findOne({ email: decodedData.email });
 
-//   if (!user)
-//     return res.status(404).json({
-//       message: "No user with this email",
-//     });
+  if (!user)
+    return res.status(404).json({
+      message: "No user with this email",
+    });
 
-//   if (user.resetPasswordExpire === null)
-//     return res.status(400).json({
-//       message: "Token Expired",
-//     });
+  if (user.resetPasswordExpire === null)
+    return res.status(400).json({
+      message: "Token Expired",
+    });
 
-//   if (user.resetPasswordExpire < Date.now()) {
-//     return res.status(400).json({
-//       message: "Token Expired",
-//     });
-//   }
+  if (user.resetPasswordExpire < Date.now()) {
+    return res.status(400).json({
+      message: "Token Expired",
+    });
+  }
 
-//   const password = await bcrypt.hash(req.body.password, 10);
+  const password = await bcrypt.hash(req.body.password, 10);
 
-//   user.password = password;
+  user.password = password;
 
-//   user.resetPasswordExpire = null;
+  user.resetPasswordExpire = null;
 
-//   await user.save();
+  await user.save();
 
-//   res.json({ message: "Password Reset" });
-// });
+  res.json({ message: "Password Reset" });
+});
